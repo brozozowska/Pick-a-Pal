a7a2da32720f3d0a6e7d0a1c459cbf50f345ec17
brozozowska <s_rozov@me.com>
2024-06-21 15:51:10 +0300

    Initial Commit

---
+++ Pick-a-Pal/Pick_a_PalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pick_a_PalApp.swift
+//  Pick-a-Pal
+//
+//  Created by Сергей Розов on 21.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct Pick_a_PalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
